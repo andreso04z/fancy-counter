924926fb0e8dc9b14ac90946ecbc57539e5d7301
Andrés Obón <andresdavidobon@gmail.com>
2025-09-19 10:53:55 -0500

    Added handler functions

--- src/CountButtons.jsx
+++ src/CountButtons.jsx
@@ -2,16 +2,20 @@ import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 
 export default function CountButtons({ setCount }) {
+  const handleDecrease = () => {
+    setCount((count) => count - 1);
+  };
+
+  const handleIncrease = () => {
+    setCount((count) => count + 1);
+  };
+
   return (
     <div className="button-container">
-      <button onClick={() => {
-        setCount((count) => count - 1);
-      }} className="count-btn count-btn--minus">
+      <button onClick={handleDecrease} className="count-btn count-btn--minus">
         <MinusIcon className="count-btn-icon" />
       </button>
-      <button onClick={() => {
-        setCount((count) => count + 1);
-      }} className="count-btn count-btn--plus">
+      <button onClick={handleIncrease} className="count-btn count-btn--plus">
         <PlusIcon className="count-btn-icon" />
       </button>
     </div>
