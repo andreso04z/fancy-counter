8bbd3937d5eeaf3d4bede8d5ef3515fc04d5ff49
andreso04z <andresdavidobon@gmail.com>
2025-09-20 15:23:43 -0500

    Added an upper limit and an app lock

--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -1,6 +1,6 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
-export default function CountButton({ type, setCount }) {
+export default function CountButton({ type, setCount, locked }) {
 
     const handleClick = () => {
         setCount((prev) => {
@@ -10,13 +10,14 @@ export default function CountButton({ type, setCount }) {
                 return newCount;
             } else {
                 const newCount = prev + 1;
+                if (newCount > 5) return 5;
                 return newCount;
             }
         });
     };
 
     return (
-        <button onClick={handleClick} className="count-btn count-btn--plus">
+        <button disabled={locked} onClick={handleClick} className="count-btn count-btn--plus">
             {type === 'minus' ? (
                 <MinusIcon className="count-btn-icon" /> 
             ) : (
